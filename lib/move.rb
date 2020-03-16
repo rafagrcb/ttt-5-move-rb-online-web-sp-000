e4934dadc67511af565523eabde4d9a5519ac060
Rafael Garcia <rafagrcb@gmail.com>
2020-03-16 21:23:21 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+board = [' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,4 +8,24 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, player = "X")
+   board[index] = player
+end
+
+def position_taken?(board, index)
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false 
+   else
+      return true
+   end
+end
+
+def valid_move?(board, index)
+   if index.between?(0,8) && !position_taken?(board, index)
+      return true
+   end
+end
